ce994ff492e65ff5f169c41600d3d4b9a88d7853
zarlent <alvdp@mail.ru>
2025-11-18 22:12:12 +0300

    Add files via upload

---
+++ Task.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+#define n 10
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    int A, sum = 0;
+    int arr[n];
+
+    printf("Введите %d целых чисел (положительных и отрицательных):\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Введите число A: ");
+    scanf("%d", &A);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == A) {
+            sum += i;
+        }
+    }
+
+    printf("Сумма индексов элементов, равных %d: %d\n", A, sum);
+    return 0;
+}
